e1b80e4071ff0c67c4ba7ed38da10ed5622fbafc
victor26 <victor-sousa1996@hotmail.com>
2022-03-31 15:11:46 -0300

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,4 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
-# Then exec the container's main process (what's set as CMD in the Dockerfile).
-exec "$@"
+bundle exec rails s -p ${PORT} -b '0.0.0.0'
